119759e72a8a8c726d906b1351a17b0247ab4fb7
amine kammoun <aminekammoun55@gmail.com>
2021-07-02 09:39:55 +0100

    back 1st commit

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: false,
+    default: 0
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
---
+++ server.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const apiUser = require("./routes/user");
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+mongoose
+  .connect("mongodb://localhost/teambuilding", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify:false
+  })
+  .then(() => {
+    console.log("connect to db");
+  })
+  .catch((err) => {
+    console.log("problem to connect to data base ", err);
+  });
+
+app.use(express.json());
+app.use("/user", apiUser);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
